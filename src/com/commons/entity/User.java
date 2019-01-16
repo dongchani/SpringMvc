c2f4aec43dbbb07c18e81a939e5c8c0a75da80a5
doch <1454218981@qq.com>
2019-01-16 11:41:34 +0800

    初次使用git提交Springmvc项目

--- src/com/commons/entity/User.java
+++ src/com/commons/entity/User.java
@@ -1,17 +1,38 @@
 package com.commons.entity;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
 
 @Data
-public class User {
+public class User  {
 
-  //private Integer id;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    //private Integer id;
   //private String userCode;
  // @NotNull(message = "userName属性不能为空")
   private String userName;
- // @NotNull(message = "userPassword属性不能为空")
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    // @NotNull(message = "userPassword属性不能为空")
   private String userPassword;
 
   public Ponit getPonit() {
